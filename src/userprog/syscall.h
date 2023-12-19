3edd4fd4dd58499938f9ce38f0b6ad53b89a6f92
cuongcmnr <134992497+cuongcmnr@users.noreply.github.com>
2023-12-19 10:52:25 +0700

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,5 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include <string.h>
+#include "filesys/filesys.h"
+#include "userprog/process.h"
 #include "filesys/directory.h" 
 #include "filesys/filesys.h" 
 #include "filesys/inode.h"
@@ -9,7 +12,7 @@ static inline struct dir *dir_open_name(const char *name) {
 }
 static inline bool inode_is_dir(struct inode *inode) { 
   return inode->data.is_dir; 
-} 
+}
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
