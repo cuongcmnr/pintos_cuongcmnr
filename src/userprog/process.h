97389b140acb75f21beb831f0e8e19530adae7a5
cuongcmnr <134992497+cuongcmnr@users.noreply.github.com>
2023-12-19 11:32:56 +0700

    Update process.h

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -5,8 +5,12 @@
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
-void process_exit (void);
+void process_exit (int status);
 void process_activate (void);
+int process_add_file(struct file *file); 
+int process_read_file(int fd, void *buffer, unsigned size); 
+int process_write_file(int fd, const void *buffer, unsigned size); 
+void process_close_file(int fd); 
 struct file_elem {
   struct file *file;  /* The file. */
   int fd;             /* The file descriptor. */
